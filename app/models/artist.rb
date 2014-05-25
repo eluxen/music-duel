f21cdee23850753836f2db9872234a13a7e1d87f
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-05-25 15:45:08 +0300

    add validation to artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,4 @@
 class Artist < ActiveRecord::Base
+   validates :name, presence: true, uniqueness: true
+   validates_format_of :default_song_url, :profile_photo_url, :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
 end
