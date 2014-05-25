0b5b62b4f1b44a188ea02e28bd26d1492ed85c8c
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-05-25 15:11:03 +0300

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicDuel::Application.config.secret_key_base = '3e82987e35014aea9119f6ac6d6a96292ce312a31daba961cb8cc494bd2e73d237158225a9a5950b026a5e3abc98c9e9e0cc369742a9d1aa71e723b7468927aa'
