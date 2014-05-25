b5ecc040bf8e6e3dba2c6b9e7df4ab9192ef6e0f
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-05-25 16:08:21 +0300

    add tagging

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ActiveRecord::Base
    validates :name, presence: true, uniqueness: true
    validates_format_of :default_song_url, :profile_photo_url, :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+
+  attr_accessible :tag_list
+
+   acts_as_taggable
 end
