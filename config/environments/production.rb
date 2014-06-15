4efa516b4cd6cf17ee8f3673067d9e31cad0657b
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-06-15 17:46:35 +0300

    Add google signing button styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,4 @@ MusicDuel::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.force_ssl = true
 end
