3670680b31dc8d128f7a0d8fc9472d4b09587d5c
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-06-21 13:21:55 +0300

    add duel vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,36 +2,20 @@ class VotesController < ApplicationController
   before_action :set_duel
   before_action :set_vote, only: :update
   before_action :login_required
-  before_action :owner_required
 
   # POST /votes
   # POST /votes.json
   def create
     @vote = @duel.votes.create(vote_params)
 
-    respond_to do |format|
-      if @vote
-        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @vote }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to @vote.duel, notice: 'Vote was successfully submitted.' 
   end
 
   # PATCH/PUT /votes/1
   # PATCH/PUT /votes/1.json
   def update
-    respond_to do |format|
-      if @vote.update(vote_params)
-        format.html { redirect_to @vote, notice: 'Vote was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @vote.errors, status: :unprocessable_entity }
-      end
-    end
+    vote.update(vote_params)
+    redirect_to @vote, notice: 'Vote was successfully updated.' 
   end
 
   private
@@ -46,6 +30,6 @@ class VotesController < ApplicationController
   end
 
   def vote_params
-    params.require(:vote).permit(:duel_id, :artist_id, :user_id)
+    params.permit(:duel_id, :artist_id, :user_id)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :duel
   has_one :artist
 
-  validates_uniqueness_of :username, :scope => :duel
+  validates_uniqueness_of :user_id, :scope => :duel
 end
