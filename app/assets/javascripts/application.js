9199e6214e1a6c310719a417d4e676981122c3cb
Lior Goldhamer <lior.goldhamer@klarna.com>
2014-12-07 17:04:01 +0200

    Add jquery.countdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.autocomplete
+//= require jquery.countdown.min
 //= require autocomplete-rails
 //= require bootstrap
 //= require moment
